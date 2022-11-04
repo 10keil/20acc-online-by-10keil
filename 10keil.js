42a9751fd31eae264775e73ae42b05bcabfd8169
10keil <111958351+10keil@users.noreply.github.com>
2022-11-04 12:57:21 +0300

    Create 10keil.js

---
+++ 10keil.js
@@ -0,0 +1,643 @@
+const Discord = require("discord.js");
+const 10keil = new Discord.Client();
+const 10keil2 = new Discord.Client();
+const 10keil3 = new Discord.Client();
+const 10keil4 = new Discord.Client();
+const 10keil5 = new Discord.Client();
+const 10keil6 = new Discord.Client();
+const 10keil7 = new Discord.Client();
+const 10keil8 = new Discord.Client();
+const 10keil9 = new Discord.Client();
+const 10keil10 = new Discord.Client();
+const 10keil11 = new Discord.Client();
+const 10keil12 = new Discord.Client();
+const 10keil13 = new Discord.Client();
+const 10keil14 = new Discord.Client();
+const 10keil15 = new Discord.Client();
+const 10keil16 = new Discord.Client();
+const 10keil17 = new Discord.Client();
+const 10keil18 = new Discord.Client();
+const 10keil19 = new Discord.Client();
+const 10keil20 = new Discord.Client();
+
+
+////////
+10keil.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+
+10keil2.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+10keil3.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+10keil4.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+10keil5.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+
+10keil6.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+
+10keil7.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+10keil8.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+
+10keil9.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+10keil10.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+10keil11.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+10keil12.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+10keil13.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+10keil14.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+
+10keil15.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+10keil16.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+10keil17.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+10keil18.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+
+10keil19.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+10keil20.on("message", message => {
+  if (!message.guild) return;
+  if (message.content === "wara") {
+    if (!message.author.id) return;
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply("**basha qashmar**");
+        })
+        .catch(console.log);
+    } else {
+      message.reply("**bro vc**");
+    }
+  }
+});
+
+//////// playing or dnd  streaming ////////
+
+10keil.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil2.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil2.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil3.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil3.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil4.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil4.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil5.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil5.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil6.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil6.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil7.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil7.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil8.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil8.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil9.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil9.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil10.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil10.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil11.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil11.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil12.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil12.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil13.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil13.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil14.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil14.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil15.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil15.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil16.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil16.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil17.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil17.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil18.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil18.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil19.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil19.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////// playing or dnd  streaming ////////
+
+10keil20.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    10keil20.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+(Math.random() * statuses.length)];
+    jano37.user.setActivity(STREAMING, {
+      type: "playing",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+
+
+
+//////////////// tokn lera da bne
+
+10keil.login("");
+10keil2.login("");
+10keil3.login("");
+10keil4.login("");
+10keil5.login("");
+10keil6.login("");
+10keil7.login("");
+10keil8.login("");
+10keil9.login("");
+10keil10.login("");
+10keil11.login("");
+10keil12.login("");
+10keil13.login("");
+10keil14.login("");
+10keil15.login("");
+10keil16.login("");
+10keil17.login("");
+10keil18.login("");
+10keil19.login("");
+10keil20.login("");
